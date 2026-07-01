27511f227c9ead2dbf8785283af8c357d8bef582
Vignesh Yogeshwar <softwareengineer989@gmail.com>
2026-07-01 15:23:34 +0530

    feat(postgres): add containerized PostgreSQL service with initialization scripts

---
+++ postgres/init/02_seed_data.sql
@@ -0,0 +1,24 @@
+INSERT INTO retail.customers
+VALUES
+
+(1,'John','New York','USA'),
+(2,'Alice','London','UK'),
+(3,'David','Mumbai','India');
+
+INSERT INTO retail.products
+VALUES
+
+(101,'Laptop','Electronics',950),
+
+(102,'Keyboard','Electronics',35),
+
+(103,'Chair','Furniture',120);
+
+INSERT INTO retail.orders
+VALUES
+
+(1001,1,101,1,'2026-01-10'),
+
+(1002,2,102,2,'2026-01-11'),
+
+(1003,3,103,1,'2026-01-12');
