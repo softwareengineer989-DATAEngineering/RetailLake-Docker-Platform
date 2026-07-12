2a7f9798504eecb4f5dedccfdda000bb3f4c58ca
softwareengineer989-DATAEngineering <softwareengineer989@gmail.com>
2026-07-12 16:12:06 +0530

    feat: finalize enterprise Docker platform and fresh build validation (#14)

---
+++ src/retail_lake/registry/bootstrap.py
@@ -0,0 +1,5 @@
+from retail_lake.jobs.customer_pipeline import CustomerPipeline
+
+# Future imports
+# from retail_lake.jobs.orders_pipeline import OrdersPipeline
+# from retail_lake.jobs.products_pipeline import ProductsPipeline
--- src/retail_lake/registry/pipeline_registry.py
+++ src/retail_lake/registry/pipeline_registry.py
@@ -1,3 +1,5 @@
+
+
 PIPELINE_REGISTRY = {}
 
 
